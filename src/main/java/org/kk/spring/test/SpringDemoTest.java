d2395da6dfa53c50832af57c51b0b2d524ee3ac1
khube <khubekumarbanjare@gmail.com>
2019-12-18 03:27:02 +0530

    I had added new few lines

--- src/main/java/org/kk/spring/test/SpringDemoTest.java
+++ src/main/java/org/kk/spring/test/SpringDemoTest.java
@@ -1,16 +1,24 @@
 package org.kk.spring.test;
 
 import org.kk.spring.beans.Student;
+import org.kk.spring.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class SpringDemoTest {
 
 	public static void main(String[] args) {
 		ApplicationContext context = null;
+		
 		Student student = null;
 		
-		student = context.getBean("student",Student.class);
-		System.out.println(student.getStudent());
+		context = new AnnotationConfigApplicationContext(AppConfig.class);
 		
+		student = context.getBean("student",Student.class);
+		student.getStudent();
+		
+		((AbstractApplicationContext) context).close();
 	}
 
 }
