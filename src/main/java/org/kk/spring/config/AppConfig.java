5b5b009a15259c726799073148603100258b8487
khube <khubekumarbanjare@gmail.com>
2019-12-18 03:09:06 +0530

    I have added 4 new classes

---
+++ src/main/java/org/kk/spring/config/AppConfig.java
@@ -0,0 +1,5 @@
+package org.kk.spring.config;
+
+public class AppConfig {
+
+}
---
+++ src/main/java/org/kk/spring/test/SpringDemoTest.java
@@ -0,0 +1,10 @@
+package org.kk.spring.test;
+
+public class SpringDemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
