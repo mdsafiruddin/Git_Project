bff45cdbc644cd78e35c5681fb04c7d35823e73e
khube007 <40648566+khube007@users.noreply.github.com>
2019-12-18 03:14:20 +0530

    I had added some new lines and configuration

--- src/main/java/org/kk/spring/config/AppConfig.java
+++ src/main/java/org/kk/spring/config/AppConfig.java
@@ -1,5 +1,17 @@
 package org.kk.spring.config;
 
+import java.util.Date;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@ComponentScan("org.kk.spring.beans")
 public class AppConfig {
 
+  @Bean
+	public Date createDate() {
+		return new Date();
+	}
 }
